7e44b95d54b89a2bfab74b13df1f505cf4b8b5f2
neusc <neusc829@163.com>
2018-03-29 10:53:15 +0800

    搭建redux基本结构，引入reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,8 @@
  * Date: 2018/3/19
  */
 import { combineReducers } from 'redux'
+import LibraryReducer from './LibraryReducer'
 
 export default combineReducers({
-  libraries: () => []
+  libraries: LibraryReducer
 })
