8dec1bfcd0faed2adff9ac92fc68ce507860eb30
neusc <neusc829@163.com>
2018-03-29 15:10:57 +0800

    引入ListView长列表组件

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import { ListView } from 'react-native'
 import { connect } from 'react-redux'
 
 class LibraryList extends Component {
+  constructor (props) {
+    super(props)
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    })
+    this.state = {
+      dataSource: ds.cloneWithRows(this.props.libraries)
+    }
+  }
+
+  renderRow (rowData) {
+
+  }
+
   render () {
-    return
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+      />
+    )
   }
 }
 
-export default connect()(LibraryList)
+const mapStateToProps = state => {
+  return { libraries: state.libraries }
+}
+
+export default connect(mapStateToProps)(LibraryList)
