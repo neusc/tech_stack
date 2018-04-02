f8e6e174abe05a17dbb1c33e04b6b503c2ad5adb
neusc <neusc829@163.com>
2018-04-02 15:44:47 +0800

    添加ListItem组件

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { ListView } from 'react-native'
 import { connect } from 'react-redux'
+import ListItem from './ListItem'
 
 class LibraryList extends Component {
   constructor (props) {
@@ -13,8 +14,8 @@ class LibraryList extends Component {
     }
   }
 
-  renderRow (rowData) {
-
+  renderRow (library) {
+    return <ListItem library={library} />
   }
 
   render () {
